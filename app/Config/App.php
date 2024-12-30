d4b0b4b48d2e7bb902b6ec7e3cdd04d8a2494770
Amene Katanda <81824558+ikatanda-1@users.noreply.github.com>
2024-12-30 16:57:13 +0200

    Update App.php - point to live domain

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'public $baseURL = 'https://cerberus-project.online/task_manager/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
