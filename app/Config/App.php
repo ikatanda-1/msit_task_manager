c38db97b5faed6020f402fc015bca8259042d020
Amene Katanda <81824558+ikatanda-1@users.noreply.github.com>
2024-12-30 18:16:39 +0200

    Update App.php set to public

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'public $baseURL = 'https://cerberus-project.online/task_manager/';
+    public string $baseURL = 'https://cerberus-project.online/task_manager/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
