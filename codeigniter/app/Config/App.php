86ef5f71394f308140e8df2703cef45a5128d5c8
Amene Katanda <81824558+ikatanda-1@users.noreply.github.com>
2024-12-30 21:24:50 +0200

    Update App.php set baseUrl

--- codeigniter/app/Config/App.php
+++ codeigniter/app/Config/App.php
@@ -16,10 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-<<<<<<< HEAD:codeigniter/app/Config/App.php
-    public string $baseURL = 'http://localhost/';
-=======
-    public string $baseURL = 'https://cerberus-project.online/task_manager/';
+
+    public string $baseURL = 'https://cerberus-project.online/';
 >>>>>>> 5e17307e14199cabb75bf26b1686112ddf2cb223:app/Config/App.php
 
     /**
